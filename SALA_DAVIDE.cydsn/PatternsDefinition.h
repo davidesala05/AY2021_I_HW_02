4b88fed578f26ba7a1d453098343bf4b952f6226
“Davide <DavideSala@example.com>
2020-10-11 11:22:01 +0200

    Interpretation of the patterns explaned in the comments

--- SALA_DAVIDE.cydsn/PatternsDefinition.h
+++ SALA_DAVIDE.cydsn/PatternsDefinition.h
@@ -24,10 +24,10 @@
         
         {3999, 3999, 0, 3999, 3999, 0},       //primo pattern
         {3999, 3999, 0, 1999, 1999, 1000},    //secondo pattern
-        {1000, 1999, 1000, 3999, 3999, 0},    //terzo pattern
-        {999, 999, 500, 499, 999, 500},       //quarto pattern
-        {250, 499, 250, 499, 499, 250},       //quinto pattern
-        {1999, 1999, 500, 1999, 1999, 1000},  //sesto pattern
+        {1000, 1999, 1000, 3999, 3999, 0},    //terzo pattern -------------> In this case the pattern of the R channel starts LOW, 
+        {999, 999, 500, 499, 999, 500},       //quarto pattern               to set that in the PWM I initialize the internal counter to the half of the period,
+        {250, 499, 250, 499, 499, 250},       //quinto pattern               in this way the PWM starts LOW for an half of the period (phase shift).
+        {1999, 1999, 500, 1999, 1999, 1000},  //sesto pattern                The same strategy is used for the 4-G, 5-R and 7-R channels.
         {1000, 1999, 1000, 999, 999, 500}     //settimo pattern
     };
     
--- SALA_DAVIDE.cydsn/main.c
+++ SALA_DAVIDE.cydsn/main.c
@@ -3,6 +3,17 @@
  * DAVIDE SALA
  * SECOND ASSIGNMENT
  * 
+ * Breif explanation of my interpretation of the patterns:
+ * I use the patterns provided in the README file as the PWMs (one for RED LED and one for the GREEN one).
+ * So, given the fact that the RGB LED is in a common anode configuration,
+ * when the PWM stays HIGH, the corresponding LED is in OFF state; the opposite happens in when the PWM is LOW.
+ * 
+ * Then, for instance taking into account the first pattern (at the power-on of the system),
+ * both the LEDs are OFF until the first pression of the button.
+ * 
+ * 
+ * 
+ * 
  * ========================================
 */
 #include "InterruptRoutines.h"
